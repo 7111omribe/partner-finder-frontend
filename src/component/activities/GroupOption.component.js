af6156e464c67338cc0e5af6083a44683ebb4644
omri ben david <ebdu12@gmail.com>
2024-02-18 22:23:08 +0200

    made also GroupOption abstraction

---
+++ src/component/activities/GroupOption.component.js
@@ -0,0 +1,73 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './OptionItem.css';
+import { StartTimeTab } from './StartTimeTab';
+
+function createCurrentAtendenciesTxt(item) {
+    let attendenciesNum = 0;
+    for (const attData of item['statusData']['attendencies']) {
+        attendenciesNum = attendenciesNum + attData['num'];
+    }
+    const currentAttendenciesTxt = `${attendenciesNum} הצטרפו`;
+    return currentAttendenciesTxt;
+}
+function createParticipantsRangeTxt(item) {
+    const minVal = item['postData']['minParticipantes'] ?? null;
+    const maxVal = item['postData']['maxParticipants'] ?? null;
+    if ((minVal === null) & (maxVal === null)) {
+        return 'אין הגבלת מקום'
+    }
+    if (minVal === null) {
+        return `עד ${maxVal} מקומות`
+    }
+    if (maxVal === null) {
+        return `${minVal}+ מקומות`
+    }
+    if (minVal == maxVal) {
+        return `${maxVal} מקומות`
+    }
+    return `${minVal} - ${maxVal} מקומות`
+
+}
+
+const GroupOption = ({ item }) => {
+    const title = item['activityData']['title'];
+    const description = item['activityData']['description'];
+
+    const currentAttendenciesTxt = createCurrentAtendenciesTxt(item);
+    const participantsRange = createParticipantsRangeTxt(item);
+    const timeAmount = item['activityData']['activityTime'] ?? '';
+    return (
+        <Container className={'option-item'}>
+            <Row>
+                <Col style={{ textAlign: 'center' }} className={'h5'}>
+                    {title}
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ textAlign: 'center' }}>
+                    {description}
+                </Col>
+            </Row>
+            <Row>
+                <Col md={3}>
+                    {currentAttendenciesTxt}
+                </Col>
+                <Col md={3}>
+                    {participantsRange}
+                </Col>
+                <Col md={3}>
+                    <StartTimeTab item={item} />
+                </Col>
+                <Col md={3} >
+                    {timeAmount}
+                </Col>
+            </Row>
+        </Container>
+    );
+
+
+};
+
+export default GroupOption;
--- src/component/activities/MyGroups/MyGroupOption.components.js
+++ src/component/activities/MyGroups/MyGroupOption.components.js
@@ -1,74 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import './../OptionItem.css';
-import { StartTimeTab } from './StartTimeTab';
+import GroupOption from '../GroupOption.component';
 
-function createCurrentAtendenciesTxt(item) {
-    let attendenciesNum = 0;
-    for (const attData of item['statusData']['attendencies']) {
-        attendenciesNum = attendenciesNum + attData['num'];
-    }
-    const currentAttendenciesTxt = `${attendenciesNum} הצטרפו`;
-    return currentAttendenciesTxt;
-}
-function createParticipantsRangeTxt(item) {
-    const minVal = item['postData']['minParticipantes'] ?? null;
-    const maxVal = item['postData']['maxParticipants'] ?? null;
-    if ((minVal === null) & (maxVal === null)) {
-        return 'אין הגבלת מקום'
-    }
-    if (minVal === null) {
-        return `עד ${maxVal} מקומות`
-    }
-    if (maxVal === null) {
-        return `${minVal}+ מקומות`
-    }
-    if (minVal == maxVal) {
-        return `${maxVal} מקומות`
-    }
-    return `${minVal} - ${maxVal} מקומות`
 
-}
 
 const MyGroupOption = ({ item }) => {
-    const title = item['activityData']['title'];
-    const description = item['activityData']['description'];
-
-    const currentAttendenciesTxt = createCurrentAtendenciesTxt(item);
-    const participantsRange = createParticipantsRangeTxt(item);
-    const timeAmount = item['activityData']['activityTime'] ?? '';
-
+    const isAdmin = item['creationData']['adminId'] === 1;
     return (
-        <Container className={'option-item'}>
-            <Row>
-                <Col style={{ textAlign: 'center' }} className={'h5'}>
-                    {title}
-                </Col>
-            </Row>
-            <Row>
-                <Col style={{ textAlign: 'center' }}>
-                    {description}
-                </Col>
-            </Row>
-            <Row>
-                <Col md={3}>
-                    {currentAttendenciesTxt}
-                </Col>
-                <Col md={3}>
-                    {participantsRange}
-                </Col>
-                <Col md={3}>
-                    <StartTimeTab item={item} />
-                </Col>
-                <Col md={3} >
-                    {timeAmount}
-                </Col>
-            </Row>
-        </Container>
-    );
-
-
+        <GroupOption
+            item={item}
+            style={isAdmin ? { backgroundColor: 'gold', borderWidth: '5px' } : {}}
+        />
+    )
 };
 
 export default MyGroupOption;
