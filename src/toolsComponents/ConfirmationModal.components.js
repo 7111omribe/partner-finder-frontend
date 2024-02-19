1d81cbc0ce0023a3b60794d49c699fd822734ac4
omri ben david <ebdu12@gmail.com>
2024-02-19 17:16:51 +0200

    hotFix

--- src/toolsComponents/ConfirmationModal.components.js
+++ src/toolsComponents/ConfirmationModal.components.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './ConfirmationModal.css'
 
-const ConfirmationModal = ({ onCancel, onConfirm, title, confirmTitle,cancelTitle }) => {
+const ConfirmationModal = ({ onCancel, onConfirm, title, confirmTitle, cancelTitle }) => {
   return (
     <div className="confirmation-modal-overlay">
       <div className="confirmation-modal">
         <p>{title}</p>
-        <button onClick={onCancel}>Cancel</button>
-        <button onClick={onConfirm}>Confirm</button>
+        <button onClick={onCancel}>{cancelTitle}</button>
+        <button onClick={onConfirm}>{confirmTitle}</button>
       </div>
     </div>
   );
