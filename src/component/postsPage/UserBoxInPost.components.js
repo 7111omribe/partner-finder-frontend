c97425560d910dedce493afc4a9b29064c034718
omri ben david <ebdu12@gmail.com>
2024-03-05 03:06:32 +0200

    now shows את.ה

--- src/component/postsPage/UserBoxInPost.components.js
+++ src/component/postsPage/UserBoxInPost.components.js
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
 import './PostPage.css';
+import { UserDataContext } from '../../App';
 
 
 const UserBoxInPost = ({ userData }) => {
-    const getFriendsNumTxt = (attendenciesNum) => {
+    const { userData: myUserData } = useContext(UserDataContext)
+
+    const getUserNameTxt = () => {
+        const myUserId = myUserData['user_id']
+        if (userData.userId == myUserId) {
+            return 'את/ה'
+        } else {
+            return ` יוזר ${userData.userNumInPost}`
+        }
+    }
+    const getFriendsNumTxt = () => {
+        const attendenciesNum = userData.num;
         if (attendenciesNum === 1) {
             return 'בא לבד'
         }
@@ -10,11 +23,11 @@ const UserBoxInPost = ({ userData }) => {
             return 'בא עם עוד חבר אחד'
         }
         return `בא עם עוד ${attendenciesNum - 1} אנשים`
-    }
+    };
     return ( // todo add restoring when have logs. and then add sorting function
         <div className="box-in-post">
-            <div>יוזר {userData.userNumInPost}</div>
-            <div>{getFriendsNumTxt(userData.num)}</div>
+            <div>{getUserNameTxt()}</div>
+            <div>{getFriendsNumTxt()}</div>
         </div>
     )
 }
